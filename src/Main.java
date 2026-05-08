c0a1d357592cf506ece4e753c23ab593236184a9
Gunn Bhatia <gunnbhatia07@gmail.com>
2026-05-08 17:05:53 +0530

    W2 D3 Done!

--- src/Main.java
+++ src/Main.java
@@ -28,12 +28,27 @@ public class Main {
     // Parameter: String[] frags
     // Return Type: String
     // Logic: Use a StringBuilder and a loop to combine all elements in the array into one String.
-    
-    
+    public static String buildSequence(String[] frags) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < frags.length; i++) {
+            sb.append(frags[i]);
+        }
+        return sb.toString();
+    }
+
+
+
     // TODO: Create a method named 'findMutation'
     // Parameters: String[] samples, String target
     // Return Type: int
     // Logic: Use a loop to search the array for the target. Return the index if found. 
     // Remember to use .equals() for Strings! If not found, return -1.
-
+    public static int findMutation(String[] samples, String target) {
+        for (int i = 0; i < samples.length; i++) {
+            if (samples[i].equals(target)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
